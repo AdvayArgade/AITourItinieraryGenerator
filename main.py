71faa69eea03bca6883de479b4529dd373a06dd5
AdvayArgade <advay.argade22@vit.edu>
2024-03-12 00:50:29 +0530

    Add doc conversion, remove unnecessary frontend elements.

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ import math
 import requests
 import streamlit as st
 from openai import OpenAI
-from geopy.geocoders import Nominatim
 import logging
 from dotenv import load_dotenv
 import os
+from docx import Document
+from docx.shared import Pt
+from docx.shared import Inches
+from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
+from io import BytesIO
+import geocoder
+
 
 load_dotenv('key.env')
 API_KEY = os.getenv('OPENAI_API_KEY')
@@ -69,11 +75,10 @@ def get_hotel_data(city, checkin_date, checkout_date, num_adults, num_children):
     city_dict = {}
 
     # Geocode city to get latitude and longitude
-    geolocator = Nominatim(user_agent="MyApp")
-    location = geolocator.geocode(city)
+    location = geocoder.osm(city)
     if location:
-        lat = location.latitude
-        long = location.longitude
+        lat = location.lat
+        long = location.lng
 
         # Define URL and query parameters for Booking.com API
         url = "https://booking-com.p.rapidapi.com/v1/hotels/search-by-coordinates"
@@ -107,7 +112,8 @@ def get_hotel_data(city, checkin_date, checkout_date, num_adults, num_children):
             data = response.json()
             if "result" in data:
                 city_dict[city] = []
-                st.write("Hotel search results for", city, ":")
+                st.write(f"Finding hotels for {city}")
+                # st.write("Hotel search results for", city, ":")
                 for index, hotel in enumerate(data["result"], start=1):
                     hotel_dict = {}
                     hotel_dict['hotel_name'] = hotel.get("hotel_name", "N/A")
@@ -118,7 +124,7 @@ def get_hotel_data(city, checkin_date, checkout_date, num_adults, num_children):
                         hotel_dict['price'] = "N/A"
                     hotel_dict['address'] = hotel.get("address", "N/A")
                     hotel_dict['rating'] = hotel.get("review_score", "N/A")
-                    st.write(f"{index}. {hotel_dict['hotel_name']} - Address: {hotel_dict['address']}, Price for one day: {hotel_dict['price']} AED, Rating: {hotel_dict['rating']}")
+                    # st.write(f"{index}. {hotel_dict['hotel_name']} - Address: {hotel_dict['address']}, Price for one day: {hotel_dict['price']} INR, Rating: {hotel_dict['rating']}")
                     city_dict[city].append(hotel_dict)
             else:
                 st.write(f"No hotel results found for {city}.")
@@ -152,26 +158,36 @@ def generate_itinerary(input_dict):
     dates = json.loads(output.function_call.arguments).get("date_list")
     dates.append(input_dict['end_date'])
 
+    input_dict['cities'] = cities
     all_city_dict = {}
-    # cities = ['Pune', 'Mumbai', 'Hyderabad']
-    # dates = ['2024-05-19', '2024-05-21', '2024-05-23', '2024-05-25']
+
+    city_string = ''
+    for city in cities:
+        city_string+=city + '  '
+    st.subheader("Cities: ")
+    st.write(city_string)
+
     for i in range(len(cities)):
-        st.write(cities[i], dates[i], dates[i+1], input_dict['num_adults'], input_dict['num_children'])
+        # st.write(cities[i], dates[i], dates[i+1], input_dict['num_adults'], input_dict['num_children'])
         all_city_dict.update(get_hotel_data(cities[i], dates[i], dates[i+1], input_dict['num_adults'], input_dict['num_children']))
+    input_dict['hotels_by_city'] = all_city_dict
+
 
     # Part 2: Actually generate the itinerary
-    user_message = f"Include the same cities and dates from your previous response. Design a detailed itinerary for a trip from {input_dict['src']} to {input_dict['dest']} starting from {input_dict['start_date']} and for " \
-                   f"{input_dict['num_days']} days. The budget for this trip is {input_dict['price_per_person']} INR per person. This trip is designed " \
+    user_message = f"Design a detailed itinerary for a trip from {input_dict['src']} to {input_dict['dest']} starting from {input_dict['start_date']} and for " \
+                   f"{input_dict['num_days']} days. The ordered list of cities is {cities} and of dates is {dates}. The budget for this trip is {input_dict['price_per_person']} INR per person. This trip is designed " \
                    f"for {input_dict['num_tourists']} mainly with their {input_dict['type_of_travelers']} with an average age of {input_dict['average_age']}.The " \
                    f"primary interests for activities are {input_dict['genre']}.The preferred mode(s) of travel include " \
                    f"{input_dict['mode_of_travel']}.The group prefers {input_dict['food']} food. Please structure the itinerary with a detailed " \
                    f"plan for each day, including activities, locations, weather according to the season they are " \
-                   f"travelling and estimated travel distances and times.Ensure to consider the preferences and " \
+                   f"travelling and estimated travel distances and times. Write the travel time and distance in the day's subheading. " \
+                   f"Ensure to consider the preferences and " \
                    f"interests of the group for each day's schedule. Important considerations: Factor in travel time " \
                    f"between destinations. Suggest local transportation options. Include a mix of activities that cater" \
                    f" to the group's interests. Also add distance of travel for each day and approx time " \
                    f"of travel. Also you can give a name for each day in the itinerary which will be more " \
-                   f"appealing. Keep the response descriptive and appealing"
+                   f"appealing. Keep the response descriptive and . Give a title to the itinerary. Do not suggest any activities " \
+                   f"in the first city."
 
     # Generate the travel itinerary using the modified user message
     chat_completion = client.chat.completions.create(
@@ -184,10 +200,81 @@ def generate_itinerary(input_dict):
         model="gpt-3.5-turbo",
         stream=True,
     )
-
-    # response_content = chat_completion.choices[0].message.content
+    st.subheader("Itinerary")
     response = st.write_stream(chat_completion)
+    # response = "Dummy response"
     return response, all_city_dict
+
+
+def text_to_doc(itinerary, input_dict):
+    document = Document()
+    paragraph = document.add_paragraph()
+    run = paragraph.add_run()
+    #run.add_picture("Logo.png", width=Inches(2.0))  # Adjust width as needed
+
+    # Set paragraph alignment to center
+    paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+    first_line = itinerary.split('\n')[1]
+
+    # Add the first line as a centered header
+    header = document.add_heading(level=1)
+    header_run = header.add_run(first_line)
+    header_run.font.size = Pt(16)  # Adjust font size if needed
+    header.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+    paragraph1 = document.add_paragraph()
+    run1 = paragraph1.add_run("Destination: ")
+    run1.bold = True
+    run1 = paragraph1.add_run(input_dict['dest'])
+
+    paragraph2 = document.add_paragraph()
+    run2 = paragraph2.add_run("Duration: ")
+    run2.bold = True
+    run2 = paragraph2.add_run(str(input_dict['num_days']))
+
+    paragraph3 = document.add_paragraph()
+    run3 = paragraph3.add_run("Interests: ")
+    run3.bold = True
+    run3 = paragraph3.add_run(input_dict['genre'])
+    paragraph2 = document.add_paragraph()
+    run2 = paragraph2.add_run("Commences on: ")
+    run2.bold = True
+    run2 = paragraph2.add_run(str(input_dict['start_date']))
+
+    for line in itinerary.split('\n')[2:]:
+        paragraph = document.add_paragraph()
+        for char in line:
+            if char == '*':
+                run = paragraph.add_run()
+                run.bold = True
+            else:
+                run = paragraph.add_run(char)
+
+
+    # we will put over data here
+    table_data = [
+        ["Destination", " Hotel"],
+    ]
+    for city in input_dict['cities']:
+        table_data.append([city, ''])
+
+    table = document.add_table(rows=len(table_data), cols=2)
+
+    # adding data to table
+    for i, row_data in enumerate(table_data):
+        for j, cell_data in enumerate(row_data):
+            table.cell(i, j).text = cell_data
+
+    # Apply alignment to the table
+    for row in table.rows:
+        for cell in row.cells:
+            for paragraph in cell.paragraphs:
+                paragraph.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+
+    # Define a custom table style (optional)
+    table.style = 'Table Grid'
+
+    return document
+
 
 st.cache(suppress_st_warning=True)(get_hotel_data)
 st.cache(suppress_st_warning=True)(generate_itinerary)
@@ -227,6 +314,32 @@ if st.button("Generate Itinerary", type="primary"):
     else:
         generated_itinerary, city_dict = generate_itinerary(input_dict)
         past_itineraries.append(generated_itinerary)  # Add to past itineraries
+        isGenerated = True
+        doc = text_to_doc(generated_itinerary, input_dict)
+        doc_io = BytesIO()
+        doc.save(doc_io)
+        doc_io.seek(0)
+        st.download_button(
+            label="Download Word Document",
+            data=doc_io,
+            file_name=f"{input_dict['dest']} Itinerary.docx",
+            mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+        )
+
+         # Main side panel for hotels
+        st.subheader("Hotels")
+        for city, hotels in city_dict.items():
+            city_expander = st.expander(f"{city}")
+            with city_expander:
+                for hotel in hotels:
+                    st.write(f"- {hotel['hotel_name']}")
+                    st.write(f"  Address: {hotel['address']}")
+                    st.write(f"  Price per day: {hotel['price']} INR")
+                    st.write(f"  Rating: {hotel['rating']}")
+                    # Add more details as needed (amenities, images, etc.)
+                    st.write("---")  # Separator between hotels
+
+
 
 with st.expander("Past Itineraries"):
     if past_itineraries:
